224d4c909ce8dc48a96817035bcde4dde7390f67
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-08-11 13:05:37 -0600

    Write all the required queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+/*
+  *Queries of feature 1
+*/
 --Find all animals whose name ends in "mon"
 SELECT * FROM animals WHERE name LIKE '%mon';
 
@@ -24,3 +27,24 @@ SELECT * FROM animals where name <> 'Gabumon';
 --Find all animals with a weight between 10.4kg and 17.3kg (including the animals with weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals where weight_kg BETWEEN 10.4 AND 17.3;
 
+/*
+  *Queries of feature 2
+*/
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT AVG(escape_attempts) FROM animals WHERE neutered = false;
+SELECT AVG(escape_attempts) FROM animals WHERE neutered = true;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth > '1990-12-31' AND date_of_birth < '2000-01-01' GROUP BY species;
