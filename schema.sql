627b06311dd1c2753308c21478b16477c95d347b
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-08-12 12:52:49 -0600

    Create new tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -1,14 +1,47 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL,
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     name TEXT,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
-    species TEXT
+    species TEXT,
+    PRIMARY KEY(id)
 );
 
--- Command used to add the table without recreating it
-ALTER TABLE animals ADD species TEXT;
+-- Command used to add the species column to the table without recreating it
+ALTER TABLE animals ADD COLUMN species TEXT;
+
+-- Command to modify the animals table id without recreating it
+BEGIN;
+ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE animals ADD id INT GENERATED ALWAYS AS IDENTITY NOT NULL;
+ALTER TABLE animals ADD PRIMARY KEY (id);
+COMMIT;
+
+/*
+ *New tables for Feature 3
+*/
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    full_name TEXT,
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name TEXT,
+    PRIMARY KEY(id)
+);
+
+-- Commands to modify the animals table new columns
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+-- Commands to add the foreign key 
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners(id);
